40473341e16cbb0a30f694d04af2eeb70bc0cfb9
Чмирёва Алёна <meloman.ytxd@gmail.com>
2024-11-01 16:39:35 +0500

    Add files via upload

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import './Experience.scss'
+const Experience = () => {
+  return (
+    <>
+    <section className='Experience'>
+        <div className="container">
+            <div className="Experience__title">
+                <h4>Experience</h4>
+                <h2>Where I've worked</h2>
+            </div>
+         <div className="Experience__cards">
+            <div className="Experience__card">
+              <p>2023 - Present</p>
+              <h3>NativeState</h3>
+              <p>Design Director</p>
+              <h4>Co-Founder</h4>
+            </div>
+            <div className="Experience__card">
+              <p>2017 — Present</p>
+              <h3>OH.STUDIO</h3>
+              <p>Freelance Designer</p>
+              <h4>Director</h4>
+            </div>
+            <div className="Experience__card">
+              <p>2021 — Present</p>
+              <h3>Friendly Studio</h3>
+              <p>Lead Product Designer</p>
+              <h4>Freelance</h4>
+            </div>
+            <div className="Experience__card">
+              <p>2022</p>
+              <h3>R/GA</h3>
+              <p>Senior Visual Designer</p>
+              <h4>Freelance</h4>
+            </div>
+            <div className="Experience__card">
+              <p>2021</p>
+              <h3>R/GA</h3>
+              <p>Senior Visual Designer</p>
+              <h4>Freelance</h4>
+            </div>
+            <div className="Experience__card">
+              <p>2021</p>
+              <h3>AKQA</h3>
+              <p>Senior Designer</p>
+              <h4>Freelance</h4>
+            </div>
+            <div className="Experience__card">
+              <p>2020</p>
+              <h3>AKQA</h3>
+              <p>Senior Designer</p>
+              <h4>Freelance</h4>
+            </div>
+            <div className="Experience__card">
+              <p>2023 - Present</p>
+              <h3>NativeState</h3>
+              <p>Design Director</p>
+              <h4>Co-Founder</h4>
+            </div>
+            <div className="Experience__card">
+              <p>2023 - Present</p>
+              <h3>NativeState</h3>
+              <p>Design Director</p>
+              <h4>Co-Founder</h4>
+            </div>
+            <div className="Experience__card">
+              <p>2023 - Present</p>
+              <h3>NativeState</h3>
+              <p>Design Director</p>
+              <h4>Co-Founder</h4>
+            </div>
+            <div className="Experience__card">
+              <p>2023 - Present</p>
+              <h3>NativeState</h3>
+              <p>Design Director</p>
+              <h4>Co-Founder</h4>
+            </div>
+            <div className="Experience__card">
+              <p>2023 - Present</p>
+              <h3>NativeState</h3>
+              <p>Design Director</p>
+              <h4>Co-Founder</h4>
+            </div>
+         </div>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Experience
